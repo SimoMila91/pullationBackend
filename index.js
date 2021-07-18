1bed567d63a1e1fa5f5246af9bdb16f9c11be095
Simone <simomilanesio5@gmail.com>
2021-07-18 17:05:05 +0200

    node_modules added and server created

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const express = require('express');
+const mySql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+let port = process.env.PORT; 
+
+if (port == null || port == "") {
+  port = 3000; 
+};
+
+const app = require('express');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+app.listen(port, () => { console.log(`Server started at port: ${port}`)});
